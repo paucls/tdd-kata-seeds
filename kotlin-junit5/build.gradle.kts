0489ee3ec48c57a7792d811feb314d456fa10c88
Paulo Clavijo Esteban <paucls@gmail.com>
2024-05-10 11:36:10 +0100

    Updated kotlin-junit5 to latest Kotlin and Gradle versions

--- kotlin-junit5/build.gradle.kts
+++ kotlin-junit5/build.gradle.kts
@@ -1,7 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.3.72"
+    kotlin("jvm") version "1.9.24"
+    java
 }
 
 repositories {
@@ -9,21 +8,16 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    testImplementation("org.assertj:assertj-core:3.21.0")
-    testImplementation("org.mockito:mockito-core:4.0.0")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.24")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.9.24")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testImplementation("org.assertj:assertj-core:3.25.3")
+    testImplementation("org.mockito:mockito-core:5.11.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.3.1")
 }
+
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 tasks.test {
     useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
-}
-
-// config JVM target to 1.8 for kotlin compilation tasks
-tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions.jvmTarget = "1.8"
 }
